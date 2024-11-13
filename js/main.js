2e996c9ce5b53adb04a368c51b8d6a57cbad80c6
infoarifk <inbox.arifkhan@gmail.com>
2024-11-14 02:57:45 +0600

    submit button working well

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const parentChild = document.getElementById("main-input");
+ 
+let add=0;
+
+function addCourse(){
+    
+const newDiv= document.createElement("div");
+newDiv.className = "input-area";
+
+
+newDiv.innerHTML = `
+<input type="text" name="" id="" placeholder="Course Name">
+            <input type="text" class="credit" placeholder="Credit">
+
+            <select class="grade">
+                <option value="">Select your Grade</option>
+                <option  value="A+">A+</option>
+                <option  value="A">A</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B">B</option>
+                <option value="B-">B-</option>
+                <option value="C">C</option>
+                <option value="D">D</option>
+                <option value="F">F</option>
+            </select>
+`;
+
+parentChild.appendChild(newDiv);
+
+add+=1;
+
+}
+
+function removeCourse(){
+  
+    if(add>0){
+  
+    const lastChild = parentChild.lastChild;
+
+    parentChild.removeChild(lastChild);
+    add-=1;
+
+    } 
+
+}
+
+
+
+
+
---
+++ js/result.js
@@ -0,0 +1,96 @@
+function submit(){
+
+    const credit = document.getElementsByClassName("credit");
+    const gradeSelect = document.querySelectorAll('.grade'); 
+    
+    const creditArray=[];
+    const gradeArray=[];
+    let totalPoint=0;
+    let totalCredit=0;
+
+
+    for(let i=0; i<credit.length; i++){
+
+       if(credit[i].value=="" || gradeSelect[i].value==""){
+        alert("Your input is not correct");
+        break;
+      
+       }
+       else{
+        creditArray.push(Number(credit[i].value));
+        gradeArray.push(gradeSelect[i].value);
+       }
+        
+    }
+
+    for(let i=0; i<creditArray.length; i++){
+
+        if(gradeArray[i]=="A+"){
+
+            totalPoint+=(creditArray[i]*4.00);
+            totalCredit+=creditArray[i];
+        }
+
+        if(gradeArray[i]=="A"){
+
+            totalPoint+=(creditArray[i]*3.75);
+            totalCredit+=creditArray[i];
+        }
+
+        if(gradeArray[i]=="A-"){
+
+            totalPoint+=(creditArray[i]*3.50);
+            totalCredit+=creditArray[i];
+        }
+        if(gradeArray[i]=="B+"){
+
+            totalPoint+=(creditArray[i]*3.25);
+            totalCredit+=creditArray[i];
+        }
+        if(gradeArray[i]=="B"){
+
+            totalPoint+=(creditArray[i]*3.00);
+            totalCredit+=creditArray[i];
+        }
+        if(gradeArray[i]=="B-"){
+
+            totalPoint+=(creditArray[i]*2.75);
+            totalCredit+=creditArray[i];
+        }
+        if(gradeArray[i]=="C+"){
+
+            totalPoint+=(creditArray[i]*2.50);
+            totalCredit+=creditArray[i];
+        }
+        if(gradeArray[i]=="C"){
+
+            totalPoint+=(creditArray[i]*2.25);
+            totalCredit+=creditArray[i];
+        }
+        if(gradeArray[i]=="D"){
+
+            totalPoint+=(creditArray[i]*2.00);
+            totalCredit+=creditArray[i];
+        }
+        if(gradeArray[i]=="F"){
+
+            totalPoint+=(creditArray[i]*0.00);
+            totalCredit+=creditArray[i];
+        }
+    }
+
+const finalResult = (totalPoint/totalCredit).toFixed(2);
+
+console.log(finalResult);
+
+
+
+
+
+
+
+    
+    
+ 
+
+}
